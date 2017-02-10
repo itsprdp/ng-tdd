537bffa6768c47a49274f9ff67dd5fee48daa795
Pradeep G <itsprdp@gmail.com>
2017-02-10 16:07:26 +0530

    Accordion styles fix

--- src/movie-app/results.controller.js
+++ src/movie-app/results.controller.js
@@ -1,6 +1,7 @@
 angular.module('movieApp')
   .controller('ResultsController', function($scope, $location, omdbApi) {
     var query = $location.search().q;
+
     omdbApi.search(query)
       .then(function(data) {
         $scope.results = data.Search;
